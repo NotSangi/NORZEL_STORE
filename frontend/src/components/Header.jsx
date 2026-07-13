164eea9b8f3dd72d28469cacdf0ea529abbe1dc9
puyo <puyobndla@gmail.com>
2026-07-13 18:38:41 -0500

    Se creó el componente Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,26 +3,25 @@ import Input from './Input.jsx';
 
 export default function Header() {
   
-  const botonCircular = "w-9 h-9 rounded-full shadow-md/30 inset-shadow-sm/30";
+  const botonCircular = 'w-9 h-9 rounded-full shadow-md/30 inset-shadow-sm/30 flex items-center justify-center hover:cursor-pointer';
 
   return (
-    <header className="w-full bg-white border-b border-gray-200 px-6 py-3 flex items-center justify-between shadow-md/30">
+    <header className='w-full bg-white border-b border-gray-200 px-6 py-3 flex items-center justify-between shadow-md/30'>
       <div className='flex items-center justify-between w-[33.33vw]'>
-        <button className={botonCircular}/>
+        <button className={botonCircular}><img src='./src/assets/icons/icono_logo.png' alt='logo' className='w-5 h-5'/></button>
         <div className='flex items-center justify-between gap-2'>
-          <button className={botonCircular}/>
-          <button className={botonCircular}/>
-          <button className={botonCircular}/>
-          <button className={botonCircular}/>
-          <button className={botonCircular}/>
+          <button className={botonCircular}><img src='./src/assets/icons/icono_camiseta.png' alt='camiseta' className='w-5 h-5'/></button>
+          <button className={botonCircular}><img src='./src/assets/icons/icono_blusa.png' alt='blusa' className='w-5 h-5'/></button>
+          <button className={botonCircular}><img src='./src/assets/icons/icono_gorra.png' alt='gorra' className='w-5 h-5'/></button>
+          <button className={botonCircular}><img src='./src/assets/icons/icono_zapato.png' alt='zapato' className='w-5 h-5'/></button>
         </div>
       </div>
       <div className='flex items-center justify-center w-[33.33vw]'>
-        <Input placeholder='buscar'/>
+        <Input placeholder='Buscar'/>
       </div>
       <div className='flex items-center gap-2 w-[33.33vw] justify-end'>
-        <button className={botonCircular}/>
-        <button className={botonCircular}/>
+        <button className={botonCircular}><img src='./src/assets/icons/icono_carrito.png' alt='carrito' className='w-5 h-5'/></button>
+        <button className={botonCircular}><img src='./src/assets/icons/icono_usuario.png' alt='usuario' className='w-5 h-5'/></button>
       </div>
     </header>
   );
--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function Input({placeholder}) {
   return (
-    <input type="text" placeholder={placeholder} className='flex  w-1/2 h-11 pl-12 pr-4 bg-white border border-gray-200 rounded-full'/>
+    <div className='flex items-center justify-start w-1/2 h-11 pl-4 pr-4 bg-white border border-gray-200 rounded-full gap-2 focus-within:ring-2 focus-within:outline-none focus-within:ring-gray-400 transition-all duration-300'>
+      <img src='./src/assets/icons/icono_buscar.png' alt='buscar' className='w-6 h-6'/>
+      <input type="text" placeholder={placeholder} className='w-full text-[20px] font-[Agdasima] outline-none'/>
+    </div>
   );  
 }
